5035077a4fd885b487d34af44e91cd100947e4dc
thelegend2004 <oleh.liubichenko@gmail.com>
2025-08-11 18:47:28 +0300

    feature: add split bill feature

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 const initialFriends = [
   {
-    id: 118836,
+    id: 117836,
     name: "Наталія",
-    image: "https://i.pravatar.cc/48?u=118836",
+    image: "https://i.pravatar.cc/48?u=117836",
     balance: -7,
   },
   {
-    id: 933372,
+    id: 931372,
     name: "Оксана",
-    image: "https://i.pravatar.cc/48?u=933372",
+    image: "https://i.pravatar.cc/48?u=931372",
     balance: 20,
   },
   {
-    id: 499476,
+    id: 497476,
     name: "Іван",
-    image: "https://i.pravatar.cc/48?u=499476",
+    image: "https://i.pravatar.cc/48?u=497476",
     balance: 0,
   },
 ];
@@ -32,6 +32,7 @@ function Button({ children, onClick }) {
 export default function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   const handleShowAddFriend = () => {
     setShowAddFriend((prev) => !prev);
@@ -42,33 +43,67 @@ export default function App() {
     handleShowAddFriend();
   };
 
+  const handleSelectFriend = (friend) => {
+    setSelectedFriend(friend);
+    setShowAddFriend(false);
+  };
+
+  const handleSplitBIll = (value) => {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : { ...friend }
+      )
+    );
+
+    setSelectedFriend(null);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList friends={friends} />
+        <FriendsList
+          selectedFriend={selectedFriend}
+          friends={friends}
+          onSelectFriend={handleSelectFriend}
+        />
+
         {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Закрити" : "Додати друга"}
         </Button>
       </div>
-      <FormSplitBill />
+      {selectedFriend && (
+        <FormSplitBill
+          onSplitBill={handleSplitBIll}
+          selectedFriend={selectedFriend}
+        />
+      )}
     </div>
   );
 }
 
-function FriendsList({ friends }) {
+function FriendsList({ friends, onSelectFriend, selectedFriend }) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelectFriend={onSelectFriend}
+          selectedFriend={selectedFriend}
+        />
       ))}
     </ul>
   );
 }
 
-function Friend({ friend }) {
+function Friend({ friend, onSelectFriend, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
   return (
-    <li>
+    <li className={isSelected ? "selected" : ""}>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
@@ -82,7 +117,9 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>{friend.name} та ти квити</p>}
-      <Button>Обрати</Button>
+      <Button onClick={() => onSelectFriend(isSelected ? null : friend)}>
+        {isSelected ? "Закрити" : "Обрати"}
+      </Button>
     </li>
   );
 }
@@ -131,25 +168,54 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill() {
+function FormSplitBill({ onSplitBill, selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  };
+
   return (
-    <form className="form-split-bill">
-      <h2>Розділити рахунок з другом</h2>
+    <form className="form-split-bill" onSubmit={handleSubmit}>
+      <h2>Розділити рахунок з {selectedFriend.name}</h2>
 
       <label>Рахунок💵</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>Твої витрати🫵</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
 
-      <label>Витрати друга🧑</label>
-      <input type="text" disabled />
+      <label>Витрати {selectedFriend.name}🧑</label>
+      <input type="text" disabled value={paidByFriend} />
 
       <label>Хто платить?🤑</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">Ти</option>
-        <option value="friend">Друг</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
+      <Button>Розділити рахунок</Button>
     </form>
   );
 }
