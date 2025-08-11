38c6acc22f2eaa8f4e131e7a4cb2c9588e59b2b8
thelegend2004 <oleh.liubichenko@gmail.com>
2025-08-11 14:18:53 +0300

    feature: add starter interface

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Наталія",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Оксана",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Іван",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          Ти винен {friend.name} {Math.abs(friend.balance)}₴
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} винен тобі {Math.abs(friend.balance)}₴
+        </p>
+      )}
+      {friend.balance === 0 && <p>{friend.name} і ти квити</p>}
+      <button className="button">Обрати</button>
+    </li>
+  );
+}
